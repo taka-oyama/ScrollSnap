3a4e15f2ec75b404d258163aa337ec9266d2568e
taka.oyama <taka.oyama@gmail.com>
2015-11-09 01:03:58 +0900

    move examples to folder

---
+++ Assets/Examples/Scripts/AddElement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AddElement : MonoBehaviour {
+	[SerializeField] LayoutElement layoutElementPrefab;
+
+	public void Awake() {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(() => {
+			var canvas = GetComponentInParent<Canvas>();
+			var snap = canvas.GetComponentInChildren<ScrollSnap>();
+			snap.PushLayoutElement(Instantiate(layoutElementPrefab));
+		});
+	}
+}
