94acabdab8a2ec5db44da06b0f3a7b2e4d7cf8ba
taka.oyama <taka.oyama@gmail.com>
2015-11-09 01:03:35 +0900

    allow push pop of elements

--- Assets/ScrollSnap.cs
+++ Assets/ScrollSnap.cs
@@ -12,8 +12,8 @@ public class ScrollSnap : UIBehaviour, IDragHandler, IEndDragHandler {
 	[SerializeField] public float triggerPercent = 10f;
 	[Range(0f, 10f)] public float triggerAcceleration = 1f;
 	
-	public delegate void LerpDelegate(Vector2 targetPosition, float transition);
-	public LerpDelegate onLerp;
+	public delegate void ChangeDelegate(int newIndex);
+	public ChangeDelegate onIndexChanged;
 
 	ScrollRect scrollRect;
 	RectTransform content;
@@ -29,10 +29,8 @@ public class ScrollSnap : UIBehaviour, IDragHandler, IEndDragHandler {
 		this.scrollRect = GetComponent<ScrollRect>();
 		this.content = scrollRect.content;
 		this.cellSize = content.GetComponent<GridLayoutGroup>().cellSize;
-		// enforce content width
-		content.sizeDelta = new Vector2(cellSize.x * ElementCount(), content.rect.height);
-		// setup initial position
-		content.anchoredPosition = new Vector2(-cellSize.x * currentIndex, content.anchoredPosition.y);
+		content.anchoredPosition = new Vector2(-cellSize.x * currentIndex, content.anchoredPosition.y);		
+		SetContentSize(LayoutElementCount());
 	}
 
 	void LateUpdate() {
@@ -42,6 +40,39 @@ public class ScrollSnap : UIBehaviour, IDragHandler, IEndDragHandler {
 				isLerping = false;
 			}
 		}
+	}
+	
+	public void PushLayoutElement(LayoutElement element) {
+		element.transform.SetParent(content.transform, false);
+		SetContentSize(LayoutElementCount());
+	}
+	
+	public void PopLayoutElement() {
+		LayoutElement[] elements = content.GetComponentsInChildren<LayoutElement>();
+		Destroy(elements[elements.Length - 1].gameObject);
+		SetContentSize(LayoutElementCount() - 1);
+		if(currentIndex == CalculateMaxIndex()) {
+			currentIndex -= 1;
+		}
+	}
+	
+	public void UnshiftLayoutElement(LayoutElement element) {
+		currentIndex += 1;
+		element.transform.SetParent(content.transform, false);
+		element.transform.SetAsFirstSibling();
+		SetContentSize(LayoutElementCount());
+		content.anchoredPosition = new Vector2(content.anchoredPosition.x - cellSize.x, content.anchoredPosition.y);		
+	}
+	
+	public void ShiftLayoutElement() {
+		Destroy(GetComponentInChildren<LayoutElement>().gameObject);
+		SetContentSize(LayoutElementCount() - 1);
+		currentIndex -= 1;
+		content.anchoredPosition = new Vector2(content.anchoredPosition.x + cellSize.x, content.anchoredPosition.y);		
+	}
+	
+	public int LayoutElementCount() {
+		return content.GetComponentsInChildren<LayoutElement>().Length;
 	}
 	
 	public void OnDrag(PointerEventData data) {
@@ -62,6 +93,9 @@ public class ScrollSnap : UIBehaviour, IDragHandler, IEndDragHandler {
 			if(newIndex >= 0 && newIndex <= CalculateMaxIndex()) {
 				currentIndex = newIndex;
 			}
+			if(onIndexChanged != null) {
+				onIndexChanged(currentIndex);
+			}
 		}
 		LerpToIndex(currentIndex);
 	}
@@ -76,7 +110,7 @@ public class ScrollSnap : UIBehaviour, IDragHandler, IEndDragHandler {
 
 	int CalculateMaxIndex() {
 		int cellPerFrame = Mathf.FloorToInt(scrollRect.viewport.sizeDelta.x / cellSize.x);
-		return ElementCount() - cellPerFrame;
+		return LayoutElementCount() - cellPerFrame;
 	}
 
 	bool IndexShouldChangeFromDrag(PointerEventData data) {
@@ -94,21 +128,18 @@ public class ScrollSnap : UIBehaviour, IDragHandler, IEndDragHandler {
 	void LerpToElement() {
 		float t = (float)((DateTime.Now - lerpStartedAt).TotalMilliseconds / lerpTimeMilliSeconds);
 		float newX = Mathf.Lerp(releasedPosition.x, targetPosition.x, t);
-		if(onLerp != null) {
-			onLerp(targetPosition, t);
-		}
 		content.anchoredPosition = new Vector2(newX, content.anchoredPosition.y);
 	}
-
+	
+	void SetContentSize(int elementCount) {
+		content.sizeDelta = new Vector2(cellSize.x * elementCount, content.rect.height);
+	}
+	
 	Vector2 CalculateTargetPoisition(int index) {
 		return new Vector2(-cellSize.x * index, content.anchoredPosition.y);
 	}
 
 	bool ShouldStopLerping() {
 		return Mathf.Approximately(content.anchoredPosition.x, targetPosition.x);
-	}
-
-	int ElementCount() {
-		return content.GetComponentsInChildren<LayoutElement>().Length;
 	}
 }
